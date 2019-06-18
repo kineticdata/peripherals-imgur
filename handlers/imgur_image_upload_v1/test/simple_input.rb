9e47ce1e80a871d1a0d2fc481c44a7d706c63ea4
rehmc <chad.rehm@kineticdata.com>
2019-06-18 09:20:11 -0500

    initial commit

---
+++ handlers/imgur_image_upload_v1/test/simple_input.rb
@@ -0,0 +1,26 @@
+{
+  'info' =>
+  {
+    'refresh_token' => '',
+    'client_id' => '',
+    'client_secret' => '',
+    'remedy_server' => '',
+    'remedy_username' => '',
+    'remedy_password' => '',
+    'remedy_authentication' => '',
+    'remedy_port' => '',
+    'remedy_prognum' => '',
+    'enable_debug_logging' => ''
+  },
+  'parameters' =>
+  {
+    'title' => '',
+    'description' => '',
+    'album' => '',
+    'attachment_question_menu_label' => ''
+    },
+  'base' => {
+    'CustomerSurveyInstanceId' => '',
+    'surveyTemplateInstanceID' => ''
+    }
+}
